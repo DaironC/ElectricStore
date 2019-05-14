beec2a6cbc7de3706a7f597c25616c1db4c39e6b
Dairon <dc1094@hotmail.com>
2019-05-14 01:27:08 -0500

    Se realizo la conexion a la bd y se creo un formulario temporal luego lo organizo y ya guarda la informacion y se implemento el for para listar el producto con imagen,nombre,precio y descripcion en la pagina store

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template,redirect,url_for,request
+from flask import Flask, render_template,redirect,url_for, request
+
 import pymysql #biblioteca myslq
 
 conn = pymysql.connect('localhost','root','','bdelectricstore')
@@ -6,12 +7,39 @@ conn = pymysql.connect('localhost','root','','bdelectricstore')
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('store.html')
+def store():
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM productos")
+    producto = cur.fetchall()
+    print(producto)
+    return render_template('store.html', producto=producto)
 
 @app.route('/agregar')
 def agregar():
     return render_template('agregarProducto.html')
+
+@app.route('/agregarProducto' ,methods=['POST'])
+def agregarProducto():
+    if request.method == "POST":
+        nombre = request.form['nombre']
+        precio = request.form['precio']
+        descripcion = request.form['descripcion']
+        imagen = request.form['imagen']
+
+        #pintar en consola los productos de la bd
+        print ("Nombre:" ,nombre)
+        print ("Precio:" ,precio)
+        print ("Descripcion:" ,descripcion)
+        print ("Imagen:" ,imagen)
+
+        #conexion a bd
+        cur = conn.cursor()
+        cur.execute("INSERT INTO productos(nombre,precio,descripcion,imagen)VALUES(%s,%s,%s,%s)",(nombre,precio,descripcion,imagen))
+        conn.commit()
+        cur.close()
+    #Despues de Guardar redirige a la pagina store    
+    return redirect(url_for('store'))
+
 
 if __name__== '__main__':
     app.run(port=4100,debug=True)
