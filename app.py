6c8f12e46377703bf20482cbfa7d6df570c4c4bb
Dairon <dc1094@hotmail.com>
2019-05-13 20:13:07 -0500

    al dia

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template,redirect,url_for,request
+import pymysql #biblioteca myslq
+
+conn = pymysql.connect('localhost','root','','bdelectricstore')
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/agregar')
+def agregar():
+    return render_template('agregarProducto.html')
 
 if __name__== '__main__':
     app.run(port=4100,debug=True)
