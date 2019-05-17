dc942f5c03042bc4fd4e91c9e92619b2f9ebe455
Dairon <dc1094@hotmail.com>
2019-05-16 19:00:34 -0500

    nueva version con el crud completo

--- app.py
+++ app.py
@@ -40,6 +40,41 @@ def agregarProducto():
     #Despues de Guardar redirige a la pagina store    
     return redirect(url_for('store'))
 
+@app.route('/update/<id>')
+def update(id):
+    cur = conn.cursor()
+    cur.execute("SELECT * from productos where id={0}".format(id))
+    producto = cur.fetchall()
+    return render_template('update_producto.html', producto=producto[0])
+
+@app.route('/update_producto/<id>', methods=['POST'])
+def update_producto(id):
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        precio = request.form['precio']
+        descripcion = request.form['descripcion']
+        imagen = request.form['imagen']
+        cur = conn.cursor()
+        cur.execute("""
+        UPDATE productos
+        set nombre = '{0}',
+            precio = '{1}',
+            descripcion = '{2}',
+            imagen = '{3}'
+        where id = {4}
+        """.format(nombre, precio, descripcion, imagen, id))
+        cur.close()
+    return redirect(url_for('store'))
+
+@app.route('/delete/<id>')
+def delete(id):
+    cur = conn.cursor()
+    cur.execute("DELETE from productos where id={0}".format(id))
+    conn.commit()
+    cur.close()
+    return redirect(url_for('store'))
+
+
 
 if __name__== '__main__':
     app.run(port=4100,debug=True)
